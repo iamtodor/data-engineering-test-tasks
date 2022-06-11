59286c4e5e4cc95a9e6fcff8a1d119800b7dac10
iamtodor <todor.ilya@gmail.com>
2022-06-11 18:51:30 +0200

    clean up

--- task5/src/etl_job.py
+++ task5/src/etl_job.py
@@ -1,36 +1,35 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql import functions as F
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql.types import StructType, StructField, StringType, DateType
 
 
 class Job:
     def __init__(self) -> None:
-        self.spark = (
+        self.__spark = (
             SparkSession.builder.master("local[*]").config("spark.driver.memory", "2g").appName("task4").getOrCreate()
         )
-        self.spark.sparkContext.setLogLevel("WARN")
-        self.input_directory = "../data/input"
-        self.dimensions = ["datehour", "domain", "user.country"]
-        self.metrics = [
-            "pageviews",
-            # 'pageviews_with_consent',
-            "consents_asked",
-            # 'consents_asked_with_consent',
-            "consents_asked_with_consent",
-            # 'consents_given_with_consent',
-            "avg_pageviews_per_user",
-        ]
+        self.__spark.sparkContext.setLogLevel("WARN")
+        self.__input_directory = "../data/input"
+        self.__dimensions = ["datehour", "domain", "user.country"]
         self.__init_schema()
 
     def __init_schema(self) -> None:
         self.__schema = StructType(
             [
-                StructField("datetime", StringType(), True),
+                StructField("datetime", DateType(), True),
                 StructField("id", StringType(), True),
                 StructField("type", StringType(), True),
                 StructField("domain", StringType(), True),
-                StructField("gender", StringType(), True),
-                StructField("salary", IntegerType(), True),
+                StructField(
+                    "user",
+                    StructType(
+                        [
+                            StructField("id", StringType(), True),
+                            StructField("token", StringType(), True),
+                            StructField("country", StringType(), True),
+                        ]
+                    ),
+                ),
             ]
         )
 
@@ -38,23 +37,19 @@ class Job:
         data = self.__load_data()
         data = self.drop_duplicates(data)
         data = self.extract_event_type(data, "pageviews", "pageview")
-        data = self.extract_event_type(data, "consents_asked", "consents.asked")
-        data = self.extract_event_type(data, "consents_given", "consents.given")
-        # data = self.calculate_avg_pageviews_per_user(data)
-        data.show(truncate=False)
-        data.printSchema()
+        data = self.extract_event_type(data, "consents_asked", "consent.asked")
+        data = self.extract_event_type(data, "consents_given", "consent.given")
+        data = self.aggregate(data)
 
-        output = data.groupBy(self.dimensions).agg(
-            F.count("pageviews").alias("pageviews"),
-            F.count("consents_asked").alias("consents_asked"),
-            F.count("consents_given").alias("consents_given"),
-            F.round(F.avg("pageviews"), 2).alias("avg_pageviews_per_user"),
-        )
-
-        self.save_data(output)
+        self.save_data(data)
 
     def __load_data(self) -> DataFrame:
-        data = self.spark.read.format("json").option("inferSchema", "true").load(f"{self.input_directory}")
+        data = (
+            self.__spark.read.format("json")
+            .option("inferSchema", "true")
+            .schema(self.__schema)
+            .load(f"{self.__input_directory}")
+        )
         return data
 
     @staticmethod
@@ -68,3 +63,11 @@ class Job:
     @staticmethod
     def save_data(result: DataFrame) -> None:
         result.coalesce(1).write.format("parquet").option("header", "true").mode("overwrite").save("../data/output")
+
+    def aggregate(self, data: DataFrame) -> DataFrame:
+        return data.groupBy(self.__dimensions).agg(
+            F.count("pageviews").alias("pageviews"),
+            F.count("consents_asked").alias("consents_asked"),
+            F.count("consents_given").alias("consents_given"),
+            F.round(F.avg("pageviews"), 2).alias("avg_pageviews_per_user"),
+        )
